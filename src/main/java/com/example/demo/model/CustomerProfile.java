b6898b0ad92fb017b9303188076dfc20544e15db
bala-amypo <bala@amypo.in>
2025-12-18 08:42:39 +0000

    User commit

--- src/main/java/com/example/demo/model/CustomerProfile.java
+++ src/main/java/com/example/demo/model/CustomerProfile.java
@@ -1,16 +1,106 @@
 package com.example.demo.model;
-import jakarta.persisitence.*;
-import java.util.LocalDate;
-public class CustomerProfile{
-    @id
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "customer_profiles")
+public class CustomerProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @column(unique=true)
+
+    @Column(unique = true, nullable = false)
     private String customerId;
+
     private String fullName;
-    @column(unique=true)
+
+    @Column(unique = true)
     private String email;
+
     private String phone;
+
     private String currentTier;
+
     private Boolean active;
-    private LocalDate createdAt;
+
+    private LocalDateTime createdAt;
+
+    public CustomerProfile() {
+    }
+    public CustomerProfile(Long id, String customerId, String fullName,String email, String phone, String currentTier,Boolean active, LocalDateTime createdAt) {
+        this.id = id;
+        this.customerId = customerId;
+        this.fullName = fullName;
+        this.email = email;
+        this.phone = phone;
+        this.currentTier = currentTier;
+        this.active = active;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCurrentTier() {
+        return currentTier;
+    }
+
+    public void setCurrentTier(String currentTier) {
+        this.currentTier = currentTier;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
---
+++ src/main/java/com/example/demo/model/PurchaseRecord.java
@@ -0,0 +1,7 @@
+public class PurchaseRecord{
+    private Long id;
+    private String customerId;
+    private Double amount;
+    private LocalDate email;
+
+}

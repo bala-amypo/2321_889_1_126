ca0f508055d23e6960b591c9b3183b87cbfce830
bala-amypo <bala@amypo.in>
2025-12-18 09:47:39 +0000

    User commit

--- src/main/java/com/example/demo/repository/TierHistoryRecordRepository.java
+++ src/main/java/com/example/demo/repository/TierHistoryRecordRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.TierHistoryRecord;
-public interface TierHistoryRecordRepository extends JpaRepository<Record,Long>{
+public interface TierHistoryRecordRepository extends JpaRepository<TierHistoryRecord,Long>{
 
 }
---
+++ src/main/java/com/example/demo/repository/TierUpgradeRuleRepository.java
@@ -0,0 +1,6 @@
+package com.example.demo.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.model.TierUpgradeRule;
+public interface TierUpgradeRuleRepository extends JpaRepository<TierUpgradeRule,Long>{
+
+}

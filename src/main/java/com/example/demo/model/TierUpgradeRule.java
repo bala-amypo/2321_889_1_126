e2a93c48074c4080aa08d41ebf380e38312a2e3b
bala-amypo <bala@amypo.in>
2025-12-18 09:02:39 +0000

    User commit

--- src/main/java/com/example/demo/model/TierUpgradeRule.java
+++ src/main/java/com/example/demo/model/TierUpgradeRule.java
@@ -1,1 +1,11 @@
-public class Tier
+package com.example.demo.model;
+import jakarta.persistence.*;
+public class TierUpgradeRule{
+    @Id
+    private Long id;
+    private String fromTier;
+    private String toTier;
+    private Double minSpend;
+    private Integer minVisits;
+    private Boolean active;
+}

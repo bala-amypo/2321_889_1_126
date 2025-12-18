cc9bb4055d74b0cd64388ee193ed644680f45915
bala-amypo <bala@amypo.in>
2025-12-18 15:53:13 +0000

    User commit

--- src/main/java/com/example/demo/repository/CustomerProfileRepository.java
+++ src/main/java/com/example/demo/repository/CustomerProfileRepository.java
@@ -1,6 +1,12 @@
 package com.example.demo.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.CustomerProfile;
-public interface CustomerProfileRepository extends JpaRepository<CustomerProfile,Long>{
 
+import com.example.demo.model.CustomerProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CustomerProfileRepository
+        extends JpaRepository<CustomerProfile, Long> {
+
+    Optional<CustomerProfile> findByCustomerId(String customerId);
 }
--- src/main/java/com/example/demo/repository/PurchaseRecordRepository.java
+++ src/main/java/com/example/demo/repository/PurchaseRecordRepository.java
@@ -1,6 +1,12 @@
 package com.example.demo.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.PurchaseRecord;
-public interface PurchaseRecordRepository extends JpaRepository<PurchaseRecord,Long>{
 
+import com.example.demo.model.PurchaseRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PurchaseRecordRepository
+        extends JpaRepository<PurchaseRecord, Long> {
+
+    Optional<PurchaseRecord> findByCustomerId(String customerId);
 }

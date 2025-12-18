9c493dbae26e9eeb4f3dd5f30def3d57f7e045d4
bala-amypo <bala@amypo.in>
2025-12-18 09:42:39 +0000

    User commit

--- src/main/java/com/example/demo/repository/CustomerProfileRepository.java
+++ src/main/java/com/example/demo/repository/CustomerProfileRepository.java
@@ -1,1 +1,6 @@
-public interface CustomerProfileRepo extends JpaRepository<
+package com.example.demo.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.model.CustomerProfile;
+public interface CustomerProfileRepository extends JpaRepository<CustomerProfile,Long>{
+
+}

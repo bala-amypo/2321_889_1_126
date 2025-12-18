cbb7b719913c943c4d0f0b90b3ee879a22c630ad
bala-amypo <bala@amypo.in>
2025-12-18 08:12:39 +0000

    User commit

--- src/main/java/com/example/demo/model/CustomerProfile.java
+++ src/main/java/com/example/demo/model/CustomerProfile.java
@@ -1,3 +1,10 @@
 public class CustomerProfile{
-    
+    private Long id;
+    private String customerId;
+    private String fullName;
+    private String email;
+    private String phone;
+    private String currentTier;
+    private Boolean active;
+    private LocalDate createdAt;
 }

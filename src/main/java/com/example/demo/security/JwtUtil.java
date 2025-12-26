0e13c8b17c01c0e6a836c0ae33e1834b5fb0ce2a
bala-amypo <bala@amypo.in>
2025-12-26 13:28:46 +0000

    User commit

---
+++ src/main/java/com/example/demo/security/JwtUtil.java
@@ -0,0 +1,33 @@
+package com.example.demo.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
+public class JwtUtil {
+
+    private final String secretKey = "amyposupersecretkey";
+    private final long expirationMs = 3600000;
+
+    public String generateToken(Long id, String email, String role) {
+        return Jwts.builder()
+                .claim("id", id)
+                .claim("email", email)
+                .claim("role", role)
+                .setIssuedAt(new Date())
+                .setExpiration(
+                        new Date(System.currentTimeMillis() + expirationMs)
+                )
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public Claims validateToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
---
+++ src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
@@ -0,0 +1,21 @@
+package com.example.demo.servlet;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/status")
+public class SimpleStatusServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req,
+                         HttpServletResponse resp)
+            throws IOException {
+
+        resp.setContentType("text/plain");
+        resp.getWriter().write("Application is running");
+    }
+}

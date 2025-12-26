a14b49adc1367b487494e7bedf7d44b9ed568cc3
bala-amypo <bala@amypo.in>
2025-12-26 13:50:22 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
@@ -1,21 +1,21 @@
 package com.example.demo.servlet;
 
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/status")
 public class SimpleStatusServlet extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest req,
-                         HttpServletResponse resp)
-            throws IOException {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
-        resp.setContentType("text/plain");
-        resp.getWriter().write("Application is running");
-    }
+        response.setContentType("text/plain");
+        response.getWriter().write("Application is running");
+    }
 }
+
